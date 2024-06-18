91f9a66d93ee4ee90ae227e9c7d729a8606e04f2
PhamDucHau2311 <phaucau2311@gmail.com>
2024-06-18 09:37:39 +0700

    update env

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(
+      `mongodb+srv://phaucau2311:0168470209haumon@chat-app.srjik2y.mongodb.net/?retryWrites=true&w=majority&appName=chat-app`
+    );
     const connection = mongoose.connection;
     connection.on("connected", () => {
       console.log("Connect to DB");
