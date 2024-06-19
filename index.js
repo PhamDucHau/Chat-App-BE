d5e0ce9b9a7eb91b210c683c38e6b5de38887aed
PhamDucHau2311 <phaucau2311@gmail.com>
2024-06-19 11:58:29 +0700

    fix cors

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const cookiesParser = require("cookie-parser");
 const { app, server } = require("./socket/index");
 
 // const app = express();
-app.use(
-  cors({
-    origin: `http://localhost:3000`,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: `http://localhost:3000`,
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(express.json());
 app.use(cookiesParser());
 
